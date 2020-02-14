e19ede3050392e21e5e5ef4d783a83a9a8adb779
Alessio Falai <falai.alessio@gmail.com>
2020-02-14 16:19:52 +0100

    add basic functions

--- inspection.py
+++ inspection.py
@@ -44,9 +44,9 @@ def show_image(image: np.ndarray, window_name: str, wait: bool = True) -> None:
 
 
 def show_connected_components(labels, window_name):
-	'''
-	Show connected components using pseudo-colors
-	'''
+    '''
+    Show connected components using pseudo-colors
+    '''
     label_hue = np.uint8(179 * labels / np.max(labels))
     blank_ch = 255 * np.ones_like(label_hue)
     labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
@@ -55,12 +55,108 @@ def show_connected_components(labels, window_name):
     show_image(labeled_img, window_name)
 
 
-def get_contours(img):
+def get_components_coords(labels):
     '''
-    Find the contours of the given connected components
+    Compute connected components coordinates
     '''
-	pass
-    
+    components_coords = []
+    num_labels = np.max(labels) + 1
+    for i in range(num_labels):
+        components_coords.append(np.argwhere(labels == i))
+    return components_coords
+
+
+def get_moments(components_coords):
+    '''
+    Compute connected components moments
+    '''
+    num_labels = len(components_coords)
+    moments = [None] * num_labels
+    for i in range(1, num_labels):
+        moments[i] = cv2.moments(components_coords[i])
+    return moments
+
+
+def get_blobs_orientation(moments):
+    '''
+    Compute blobs orientation from central moments
+    '''
+    angles = [None] * len(moments)
+    for i, blob_moments in enumerate(moments):
+        if blob_moments is not None:
+            major_axis_angle = -0.5 * np.arctan(
+                blob_moments['mu11'] /
+                (blob_moments['mu02'] + blob_moments['mu20'] + 1e-5)
+            )
+            minor_axis_angle = major_axis_angle + (np.pi / 2)
+            angles[i] = {
+                'major': major_axis_angle,
+                'minor': minor_axis_angle
+            }
+    return angles
+
+
+def get_blobs_mer(components_coords):
+    '''
+    Compute blobs minimum enclosing oriented rectangle
+    '''
+    blobs_mer = [None]
+    for _, coords in enumerate(components_coords):
+        blobs_mer.append(
+            np.int0(cv2.boxPoints(cv2.minAreaRect(coords)))
+        )
+    return blobs_mer
+
+
+def get_blobs_straight_bbox(components_coords):
+    '''
+    Compute blobs straight bounding rectangle
+    '''
+    blobs_bbox = [None]
+    for _, coords in enumerate(components_coords):
+        blobs_bbox.append(cv2.boundingRect(coords))
+    return blobs_bbox
+
+
+def show_blobs_mer(img, blobs_mer, window_name):
+    '''
+    Show the given oriented rectangles on the given image
+    '''
+    image = img.copy()
+    for mer in blobs_mer:
+        if mer is not None:
+            cv2.drawContours(image, [mer], 0, (0, 0, 255), 1)
+    show_image(image, window_name)
+
+
+def show_blobs_straight_bbox(img, blobs_bbox, window_name):
+    '''
+    Show the given straight rectangles on the given image
+    '''
+    image = img.copy()
+    for bbox in blobs_bbox:
+        if bbox is not None:
+            x, y, w, h = bbox
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 1)
+    show_image(image, window_name)
+
+
+def show_blobs_axis(img, angles, centroids):
+    '''
+    '''
+    image = img.copy()
+    for i, centroid in enumerate(centroids):
+        if angles[i] is not None:
+            major_axis_angle = angles[i]['major']
+            point = (
+                centroid + img.shape[0] *
+                np.array([np.cos(major_axis_angle), np.sin(major_axis_angle)])
+            )
+            cv2.line(
+                image, (int(centroid[0]), int(centroid[1])),
+                (int(point[0]), int(point[1])), (0, 0, 255), 1
+            )
+    show_image(image, "TEST")
 
 
 def main():
@@ -68,10 +164,19 @@ def main():
     _, threshed = cv2.threshold(
         img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
     )
-    # show_image(threshed, "Threshed")
-    ret, labels = cv2.connectedComponents(threshed)
-    # show_connected_components(labels, "Connected components")
-    get_holes(threshed)
+    show_image(threshed, "Threshed")
+    _, labels, stats, centroids = cv2.connectedComponentsWithStats(threshed)
+    print(centroids)
+    show_connected_components(labels, "Connected components")
+    components_coords = get_components_coords(labels)
+    print(components_coords)
+    moments = get_moments(components_coords)
+    angles = get_blobs_orientation(moments)
+    # blobs_mer = get_blobs_mer(components_coords)
+    # show_blobs_mer(threshed, blobs_mer, "MER")
+    # blobs_bbox=get_blobs_straight_bbox(components_coords)
+    # show_blobs_straight_bbox(threshed, blobs_bbox, "BBOX")
+    show_blobs_axis(threshed, angles, centroids)
 
 
 if __name__ == '__main__':
